ccced3793dc4adf92b62f7006e825a2b6a1390b4
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-13 15:17:26 +0100

    Implement Nav

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Nav from "../common/components/nav";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default Home;
