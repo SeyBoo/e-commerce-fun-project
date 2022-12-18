4fcadadd4edb419378e0313ad59b0da09f3e3f07
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-18 17:23:02 +0100

    max width baseLayout

--- src/common/components/layouts/baseLayout.tsx
+++ src/common/components/layouts/baseLayout.tsx
@@ -3,10 +3,10 @@ import Nav from "../nav";
 
 const BaseLayout: FunctionComponent<PropsWithChildren> = ({ children }) => {
   return (
-    <>
+    <div className="max-w-[1860px] m-auto">
       <Nav />
       <main className="w-[90%] m-auto">{children}</main>
-    </>
+    </div>
   );
 };
 
