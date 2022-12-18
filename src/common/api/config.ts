d1059a709afb3dbc74eb6c21b2e19f57acbf6816
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-18 17:23:47 +0100

    transform queryClient to api config

--- src/common/api/config.ts
+++ src/common/api/config.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
+import { QueryClient } from "react-query";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 5000,
+});
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { cacheTime: 200, staleTime: 1000 * 60 * 30 },
+  },
 });
 
 export const getFromApi = async (
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,10 @@
 import "../common/styles/globals.css";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClientProvider } from "react-query";
 import SnackBarProvider from "../common/hooks/useSnackBar";
+import { queryClient } from "../common/api/config";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: { cacheTime: 200, staleTime: 1000 * 60 * 30 },
-    },
-  });
-
   return (
     <QueryClientProvider client={queryClient}>
       <SnackBarProvider>
