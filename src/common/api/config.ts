275fa7a5438389c8a95851c6266a0e3293f6d053
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-18 17:35:59 +0100

    remove timeout config

--- src/common/api/config.ts
+++ src/common/api/config.ts
@@ -3,7 +3,6 @@ import { QueryClient } from "react-query";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 5000,
 });
 
 export const queryClient = new QueryClient({
