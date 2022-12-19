dfa560faebbc6e6d4ce3eeb6d7933885a2ead352
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-19 10:30:29 +0100

    space between layout component

--- src/common/components/layouts/baseLayout.tsx
+++ src/common/components/layouts/baseLayout.tsx
@@ -3,9 +3,9 @@ import Nav from "../nav";
 
 const BaseLayout: FunctionComponent<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="max-w-[1860px] m-auto">
+    <div className="max-w-[1860px] m-auto flex flex-col justify-between h-screen gap-6">
       <Nav />
-      <main className="w-[90%] m-auto">{children}</main>
+      <main className="w-[90%] m-auto flex-grow">{children}</main>
     </div>
   );
 };
