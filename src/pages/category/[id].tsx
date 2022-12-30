581b98fe4652fd2ce2614e0bcb2112c18904a1a7
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-30 18:30:07 +0100

    category page style

--- src/pages/category/[id].tsx
+++ src/pages/category/[id].tsx
@@ -4,14 +4,37 @@ import {
   GetStaticPropsContext,
   NextPage,
 } from "next";
+import { useRouter } from "next/router";
 import { dehydrate, QueryClient } from "react-query";
+import GridInfiteQuery from "../../common/components/grid-infite-query";
+import BaseLayout from "../../common/components/layouts/baseLayout";
 import {
   getAllCategories,
   prefetchCategory,
+  useGetCategory,
 } from "../../module/category/api/category.api";
+import ProductCard from "../../module/products/components/productCard";
+import ProductCardSkeleton from "../../module/products/components/productCard/skeleton";
+import SearchProducts from "../../module/products/components/searchProducts";
+import { ProductI } from "../../module/products/types/products.interface";
 
 const CategoryPage: NextPage = () => {
-  return null;
+  const router = useRouter();
+  const id = router.query.id as string;
+  const query = useGetCategory(id);
+
+  return (
+    <BaseLayout>
+      <div className="flex justify-between">
+        <SearchProducts data={query.data} />
+      </div>
+      <GridInfiteQuery<ProductI>
+        query={query}
+        renderItem={(product) => <ProductCard product={product} />}
+        renderSkeleton={() => <ProductCardSkeleton />}
+      />
+    </BaseLayout>
+  );
 };
 
 export default CategoryPage;
