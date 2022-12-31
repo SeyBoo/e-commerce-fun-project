5c229b1941244a358782bf1641afa0419e72efb2
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-31 15:40:39 +0100

    cart page

---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import BaseLayout from "../../common/components/layouts/baseLayout";
+
+const Cart: NextPage = () => {
+  return (
+    <BaseLayout></BaseLayout>
+  );
+};
+
+export default Cart;
