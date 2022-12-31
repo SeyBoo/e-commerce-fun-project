edea62493d0819e616a2e06d8a61069d84737947
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-12-31 21:34:25 +0100

    show cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,9 +1,42 @@
 import { NextPage } from "next";
+import { FunctionComponent } from "react";
 import BaseLayout from "../../common/components/layouts/baseLayout";
+import { useAppSelector } from "../../common/hooks/store";
+import { ProductCart } from "../../module/cart/components/productCart";
+import NoItemIllustration from "../../common/assets/no-item.webp";
+import Image from "next/image";
 
 const Cart: NextPage = () => {
+  const cartProducts = useAppSelector((state) => state.cart.products);
+
+  const ProductsInCart: FunctionComponent = () => {
+    return (
+      <div className="flex flex-col gap-8">
+        {cartProducts?.map((product, index) => (
+          <ProductCart product={product} key={index} />
+        ))}
+      </div>
+    );
+  };
+
+  const NoItemInCard: FunctionComponent = () => {
+    return (
+      <div className="flex flex-col items-center -mt-10 text-center">
+        <Image
+          src={NoItemIllustration}
+          alt="no-item-illustration"
+          width={500}
+          height={500}
+        />
+        <p className="text-2xl">No item in card</p>
+      </div>
+    );
+  };
+
   return (
-    <BaseLayout></BaseLayout>
+    <BaseLayout>
+      {cartProducts ? <ProductsInCart /> : <NoItemInCard />}
+    </BaseLayout>
   );
 };
 
