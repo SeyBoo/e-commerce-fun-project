021a00198ec72488212a67a43ef5cd7e8eefe177
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2023-01-06 10:33:37 +0100

    parse items of nav into components

--- src/common/components/nav.tsx
+++ src/common/components/nav.tsx
@@ -14,7 +14,7 @@ import MenuDropDown from "./animations/menu";
 import { useGetAllProducts } from "../../module/products/api/products.api";
 import { useAppSelector } from "../hooks/store";
 
-const panelItems: PanelItem[] = [
+const categoriesPanel: PanelItem[] = [
   {
     name: "Electronics",
     image: ElectronicsIcon,
@@ -37,19 +37,40 @@ const Nav: FunctionComponent = () => {
   const { data: productsData } = useGetAllProducts();
   const productsCount = useAppSelector((state) => state.cart.productsCount);
 
-  return (
-    <nav className="flex items-center justify-between p-5">
-      <div className="flex xl:gap-20 gap-10 items-center">
-        <Link href="/">
-          <Image src={Logo} alt="logo" width={225} />
-        </Link>
-        <div className="hidden md:flex gap-6 font-medium ">
-          <PopOverPanel triggerButton="Categories" panelItems={panelItems} />
-          <Link href="#">Deals</Link>
-          <Link href="#">What&apos;s news</Link>
-          <Link href="#">Delivery</Link>
+
+  const renderSignInButton = () => {
+    const DefaultContent = () => (
+      <>
+        <Image src={AccountIcon} alt="account-icon" width={30} />
+        <p className="md:hidden xl:block">Account</p>
+      </>
+    );
+
+    return (
+      <Link href="/auth/signin" className="flex items-center gap-1 font-medium">
+        <DefaultContent />
+      </Link>
+    );
+  };
+
+  const renderCart = () => {
+    return (
+      <Link href="/cart" className="flex items-center gap-1 font-medium">
+        <div className="inline-flex relative items-center p-1 text-sm font-medium text-center text-white rounded-lg">
+          <Image src={CartIcon} alt="cart-icon" width={30} />
+          {productsCount !== 0 && (
+            <div className="inline-flex absolute -top-1 -right-1 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-green-700 rounded-full">
+              {productsCount}
+            </div>
+          )}
         </div>
-      </div>
+        <p className="md:hidden xl:block">Cart</p>
+      </Link>
+    );
+  };
+
+  const renderMobileMenu = () => {
+    return (
       <MenuDropDown
         sections={[
           {
@@ -61,7 +82,8 @@ const Nav: FunctionComponent = () => {
                 children: (
                   <PopOverPanel
                     triggerButton="Categories"
-                    panelItems={panelItems}
+                    panelItems={categoriesPanel}
+                    showDropDownIcon
                   />
                 ),
               },
@@ -82,55 +104,44 @@ const Nav: FunctionComponent = () => {
           {
             items: [
               {
-                children: (
-                  <Link
-                    href="/login"
-                    className="flex items-center gap-1 font-medium"
-                  >
-                    <Image src={AccountIcon} alt="" width={30} />
-                    Account
-                  </Link>
-                ),
+                children: renderSignInButton(),
                 isActiveStyle: true,
               },
               {
-                children: (
-                  <Link
-                    href="/cart"
-                    className="flex items-center gap-1.5 font-medium"
-                  >
-                    <div className="inline-flex relative items-center text-sm font-medium text-center rounded-lg">
-                      <Image src={CartIcon} alt="cart-icon" width={30} />
-                      <div className="inline-flex absolute -top-2 -right-2 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-green-700 rounded-full">
-                        {productsCount}
-                      </div>
-                    </div>
-                    <p>Cart</p>
-                  </Link>
-                ),
+                children: renderCart(),
                 isActiveStyle: true,
               },
             ],
           },
         ]}
       />
+    );
+  };
+
+  return (
+    <nav className="flex items-center justify-between p-5">
+      <div className="flex xl:gap-20 gap-10 items-center">
+        <Link href="/">
+          <Image src={Logo} alt="logo" width={225} />
+        </Link>
+        <div className="hidden md:flex gap-6 font-medium ">
+          <PopOverPanel
+            triggerButton="Categories"
+            panelItems={categoriesPanel}
+            showDropDownIcon
+          />
+          <Link href="#">Deals</Link>
+          <Link href="#">What&apos;s news</Link>
+          <Link href="#">Delivery</Link>
+        </div>
+      </div>
+      {renderMobileMenu()}
       <div className="hidden md:flex items-center gap-6 w-max">
         <div className="w-[350px]">
           <SearchProducts data={productsData} />
         </div>
-        <Link href="/login" className="flex items-center gap-1 font-medium">
-          <Image src={AccountIcon} alt="" width={30} />
-          <p className="hidden xl:block">Account</p>
-        </Link>
-        <Link href="/cart" className="flex items-center gap-1 font-medium">
-          <div className="inline-flex relative items-center p-1 text-sm font-medium text-center text-white rounded-lg">
-            <Image src={CartIcon} alt="cart-icon" width={30} />
-            <div className="inline-flex absolute -top-1 -right-1 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-green-700 rounded-full">
-              {productsCount}
-            </div>
-          </div>
-          <p className="hidden xl:block">Cart</p>
-        </Link>
+        {renderSignInButton()}
+        {renderCart()}
       </div>
     </nav>
   );
