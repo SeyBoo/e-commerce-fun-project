c832ef652529a69d30ad1aede504c9fc424b0237
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2023-01-06 10:41:07 +0100

    use useSignIn on SignIn page

--- src/module/auth/types/auth.interface.ts
+++ src/module/auth/types/auth.interface.ts
@@ -2,3 +2,7 @@ export interface SignInProps {
   username: string;
   password: string;
 }
+
+export interface PostUserPayloadResponse {
+  token: string;
+}
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -3,15 +3,37 @@ import Link from "next/link";
 import { useCustomForm } from "../../common/hooks/useCustomForm";
 import Image from "next/image";
 import Logo from "../../common/assets/logo.svg";
+import { useSnack } from "../../common/hooks/useSnackBar";
 import { SignInProps } from "../../module/auth/types/auth.interface";
+import { useSignIn } from "../../module/auth/api/auth.api";
 import { FormEvent } from "react";
 
 const SignIn: NextPage = () => {
+  const setSnackBar = useSnack();
+  const { mutate } = useSignIn();
+
   const [formValues, handleChange] = useCustomForm<SignInProps>({
     username: "mor_2314",
     password: "83r5^_",
   });
 
+  const handleSignIn = async () => {
+    try {
+      mutate({ password: formValues.password, username: formValues.username });
+    } catch (e) {
+      setSnackBar({
+        title: "Couldn't connect you.",
+        type: "error",
+      });
+    }
+  };
+
+  const handleSubmitForm = (e: FormEvent) => {
+    e.preventDefault();
+    if (formValues.password == "" || formValues.username == "") return;
+
+    handleSignIn();
+  };
 
   return (
     <div className="flex flex-col items-center gap-12 py-10">
@@ -25,6 +47,7 @@ const SignIn: NextPage = () => {
         </div>
         <form
           className="flex flex-col gap-5"
+          onSubmit={(e) => handleSubmitForm(e)}
         >
           <div className="flex flex-col gap-1.5">
             <label htmlFor="username" className="text-xs">
